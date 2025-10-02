63ef135982b0f0a899ffdd4d395c2b41042242f5
jeffryith <jeffry.ith@kawanlamacorp.com>
2025-10-02 16:47:22 +0700

    change to uuid and have foreign key

--- db/migrate/20250929061234_create_users_follow.rb
+++ db/migrate/20250929061234_create_users_follow.rb
@@ -4,8 +4,8 @@
 class CreateUsersFollow < ActiveRecord::Migration[8.0]
   def change
     create_table :users_follows do |t|
-      t.references :follower, null: false
-      t.references :followed_user, null: false
+      t.references :follower, null: false, foreign_key: { to_table: :users }, type: :uuid
+      t.references :followed_user, null: false, foreign_key: { to_table: :users }, type: :uuid
       t.timestamps
     end
   end
