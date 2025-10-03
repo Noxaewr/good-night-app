5bb8dea44ee283ce4c70f1629c1069b886522981
jeffryith <jeffry.ith@kawanlamacorp.com>
2025-10-03 14:38:24 +0700

    feat(swagger_helper): add Swagger configuration for API documentation with RSpec

---
+++ spec/swagger_helper.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'simplecov'
+SimpleCov.start 'rails'
+
+RSpec.configure do |config|
+  # Specify a root folder where Swagger JSON files are generated
+  # NOTE: If you're using the rswag-api to serve API descriptions, you'll need
+  # to ensure that it's configured to serve Swagger from the same folder
+  config.openapi_root = Rails.root.join('swagger').to_s
+
+  # Define one or more Swagger documents and provide global metadata for each one
+  # When you run the 'rswag:specs:swaggerize' rake task, the complete Swagger will
+  # be generated at the provided relative path under openapi_root
+  # By default, the operations defined in spec files are added to the first
+  # document below. You can override this behavior by adding a openapi_spec tag to the
+  # the root example_group in your specs, e.g. describe '...', openapi_spec: 'v2/swagger.json'
+  config.openapi_specs = {
+    'v1/swagger.yaml' => {
+      openapi: '3.0.1',
+      info: {
+        title: 'Good Night App API V1',
+        version: 'v1',
+        description: 'API for tracking sleep records and following users',
+        contact: {
+          name: 'API Support',
+          email: 'support@goodnightapp.com'
+        }
+      },
+      paths: {},
+      servers: [
+        {
+          url: 'http://localhost:3000',
+          description: 'Development server'
+        },
+        {
+          url: 'https://api.goodnightapp.com',
+          description: 'Production server'
+        }
+      ],
+      components: {}
+    }
+  }
+
+  # Specify the format of the output Swagger file when running 'rswag:specs:swaggerize'.
+  # The openapi_specs configuration option has the filename, format and
+  # the specifications itself, which are used to generate the Swagger file.
+  config.openapi_format = :yaml
+end
